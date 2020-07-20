dd3e1ab1e728a62ce8ac8aad02a246996e1d8834
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-20 14:55:38 +0300

    Добавьте файлы проекта.

---
+++ 13052020.cpp
@@ -0,0 +1,43 @@
+﻿﻿#include <string.h>
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+//#include "MyLibr.h"  ???????
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char group[][50] = {
+	  "Аня",
+	  "Петро",
+	  "Іван",
+	  "Іра",
+	  "Ярослав"
+	};
+	int size = 5;
+	for (size_t i = 0; i < size; i++)
+		cout << group[i] << endl;
+	cout << "------------------------------------" << endl;
+	bool fl;
+	int k = 0;
+	do {
+		fl = false;
+		for (size_t i = 0; i < size - k - 1; i++)
+		{
+			if (strcoll(group[i], group[i + 1]) > 0) {
+				swap(group[i], group[i + 1]);
+				fl = true;
+			}
+		}
+		k++;
+	} while (fl);
+
+	for (size_t i = 0; i < size; i++)
+		cout << group[i] << endl;
+	cout << "------------------------------------" << endl;
+
+
+}
